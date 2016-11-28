9d252aeb32db4713b435ffac8ed2f707049d1902
Garrett Berg <vitiral@gmail.com>
2016-11-27 22:58:08 -0700

    fix quemu stuff

--- ci/script.sh
+++ ci/script.sh
@@ -9,25 +9,26 @@ set -ex
 # to target/$TARGET/{debug,release} which can reduce the number of needed conditionals in the
 # `before_deploy`/packaging phase
 run_test_suite() {
-    case $TARGET in
-        # configure emulation for transparent execution of foreign binaries
-        aarch64-unknown-linux-gnu)
-            export QEMU_LD_PREFIX=/usr/aarch64-linux-gnu
-            ;;
-        arm*-unknown-linux-gnueabihf)
-            export QEMU_LD_PREFIX=/usr/arm-linux-gnueabihf
-            ;;
-        *)
-            ;;
-    esac
+	## DONE: disable anything related to quemu because it kills me
+    #case $TARGET in
+    #    # configure emulation for transparent execution of foreign binaries
+    #    aarch64-unknown-linux-gnu)
+    #        export QEMU_LD_PREFIX=/usr/aarch64-linux-gnu
+    #        ;;
+    #    arm*-unknown-linux-gnueabihf)
+    #        export QEMU_LD_PREFIX=/usr/arm-linux-gnueabihf
+    #        ;;
+    #    *)
+    #        ;;
+    #esac
 
-    if [ ! -z "$QEMU_LD_PREFIX" ]; then
-        # Run tests on a single thread when using QEMU user emulation
-        export RUST_TEST_THREADS=1
-    fi
+    #if [ ! -z "$QEMU_LD_PREFIX" ]; then
+    #    # Run tests on a single thread when using QEMU user emulation
+    #    export RUST_TEST_THREADS=1
+    #fi
 
     cargo build --target $TARGET --verbose
-    cargo run --target $TARGET
+    cargo run --target $TARGET -- check
     cargo test --target $TARGET
 
     # sanity check the file type
