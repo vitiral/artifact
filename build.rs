de1b1a58d9ca2972547dd3478f5debda9e69bed8
Garrett Berg <vitiral@gmail.com>
2016-11-29 17:56:54 -0700

    try windows build again

--- build.rs
+++ build.rs
@@ -5,6 +5,7 @@ pub use std::env;
 pub use std::path::Path;
 pub use std::process::Command;
 
+
 #[cfg(feature = "web")] 
 mod web {
     use std::io::Read;
@@ -13,15 +14,25 @@ mod web {
 
     use tar;
 
-    pub fn package_web_ui() {
-        let cwd = env::current_dir().unwrap();
-        let web_dir = cwd.join("web-ui");
+    #[cfg(windows)]
+    fn build_web_ui(_: &Path) {
+        println!("not building web-ui because we are on windows");
+    }
+
+    #[cfg(not(windows))]
+    fn build_web_ui(web_dir: &Path) {
         let npm_build = Command::new("npm")
             .current_dir(&web_dir)
             .args(&["run", "build"])
             .output()
             .expect("failed to build web-ui");
         println!("npm build succeeded:\n{:?}", npm_build.stdout);
+    }
+
+    pub fn package_web_ui() {
+        let cwd = env::current_dir().unwrap();
+        let web_dir = cwd.join("web-ui");
+        build_web_ui(&web_dir);
 
         let dist_dir = web_dir.join("dist");
         let tarfile = cwd.join("target").join("web-ui.tar");
