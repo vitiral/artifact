48b5596445365ba911696ab86cca5ecce6e9f926
Ruben De Smet <ruben.de.smet@rubdos.be>
2017-03-10 16:11:42 +0100

    Close #104: Add --output flag for export subcommand
    Close #98

--- src/cmd/export.rs
+++ src/cmd/export.rs
@@ -16,6 +16,12 @@ pub fn get_subcommand<'a, 'b>() -> App<'a, 'b> {
         .settings(&SUBCMD_SETTINGS)
         .arg(Arg::with_name("type")
             .help("Type of export.\n- html: static html. Writes to `./index.html` and `./css/`"))
+        .arg(Arg::with_name("output")
+            .short("o")
+            .long("output")
+            .value_name("PATH")
+            .help("The export output directory. If omitted, the current working directory \
+                  is used."))
 }
 
 #[derive(Debug)]
@@ -24,11 +30,12 @@ pub enum ExportType {
 }
 
 #[derive(Debug)]
-pub struct Cmd {
+pub struct Cmd<'a> {
     ty: ExportType,
+    output: Option<&'a Path>,
 }
 
-pub fn get_cmd(matches: &ArgMatches) -> Result<Cmd> {
+pub fn get_cmd<'a>(matches: &'a ArgMatches) -> Result<Cmd<'a>> {
     let ty = match matches.value_of("type")
               .unwrap_or("")
               .to_ascii_lowercase()
@@ -36,10 +43,15 @@ pub fn get_cmd(matches: &ArgMatches) -> Result<Cmd> {
         "html" => ExportType::Html,
         t => return Err(ErrorKind::CmdError(format!("unknown type: {}", t)).into()),
     };
-    Ok(Cmd { ty: ty })
+    let dir = matches.value_of("output").map(|x| Path::new(x).as_ref());
+    Ok(Cmd {
+           ty: ty,
+           output: dir,
+       })
 }
 
 pub fn run_cmd(cwd: &Path, project: &Project, cmd: &Cmd) -> Result<u8> {
+    let output = cmd.output.unwrap_or(cwd);
     match cmd.ty {
         ExportType::Html => {
             // get the artifacts as json and replace with escaped chars
@@ -49,14 +61,14 @@ pub fn run_cmd(cwd: &Path, project: &Project, cmd: &Cmd) -> Result<u8> {
 
             // unpack the index.html + css/ files
             let mut archive = Archive::new(WEB_STATIC_TAR);
-            if let Err(e) = fs::remove_dir_all(cwd.join("css")) {
+            if let Err(e) = fs::remove_dir_all(output.join("css")) {
                 if e.kind() == io::ErrorKind::NotFound {
                 } else {
                     return Err(e.into());
                 }
             }
-            archive.unpack(&cwd).expect("unable to unpack web frontend");
-            let index_path = cwd.join("index.html");
+            archive.unpack(&output).expect("unable to unpack web frontend");
+            let index_path = output.join("index.html");
             let mut index = fs::OpenOptions::new()
                 .read(true)
                 .write(true)
