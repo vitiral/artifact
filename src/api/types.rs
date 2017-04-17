cc8020dc47d2478413528a96b1142927fe2bef7d
Dan Kaufman <djk86@pitt.edu>
2017-04-17 10:39:53 -0400

    fixed compile errors

--- src/api/types.rs
+++ src/api/types.rs
@@ -22,8 +22,7 @@ pub struct Version {
 	pub build: Option<String>,
 }
 
-#[derive(Queryable)]
-#[table_name="version"]
+#[derive(Debug, Queryable, Serialize, Deserialize)]
 pub struct NewVersion {
 	pub major: String,
 	pub minor: Option<String>,
