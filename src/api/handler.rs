4027bb13352572cc582a9d14fec847d70c4d37dd
Austin <ajj29@pitt.edu>
2017-04-20 19:17:31 -0400

    it almost compiles!

--- src/api/handler.rs
+++ src/api/handler.rs
@@ -200,3 +200,66 @@ impl RpcMethodSync for AddTestRun {
 	}
 }
 
+/// `AddTests` API Handler
+struct AddTest;
+impl RpcMethodSync for AddTest {
+	fn call(&self, params: Params) -> result::Result<serde_json::Value, RpcError> {
+		use self::test_name::dsl::*;
+		use self::test_name;
+		use serde_json::Value as SerdeValue;
+		info!("AddTest called");
+		
+		let connection = establish_connection();
+		
+		info!("{:?}", params);
+		
+		let mut newTest: NewTestName = Default::default();
+		let new_test_name;
+		
+		match params {
+			Params::Map(mut dict) => {
+				new_test_name = match dict.remove("name") {
+					Some(value) => {
+						info!("{:?}", value);
+						match serde_json::from_value::<String>(value) {
+							Ok(a) => Ok(a),
+							Err(e) => Err(utils::parse_error(&format!("{}", e))),
+						}
+					}
+					None => Err(utils::invalid_params("Error: missing 'name' param in AddTest API call")),
+				}
+			}
+			_ => panic!("Error: AddTest API call missing 'name' key"),
+		};
+		
+        #[derive(Deserialize)]
+        struct CheckName{
+            name: String,
+        }
+        //Checking if the test name is already in the test_name table
+        let serialized_params = serde_json::to_value(params).unwrap();
+        let mut check_duplicate: CheckName = serde_json::from_value(serialized_params).unwrap();
+        let results = test_name.filter(test_name::name.eq(&check_duplicate.name)).load::<TestName>(&connection)
+            .expect("Error loading test name table");
+        // for test_name in results{
+
+        // }
+        if(results.is_empty()){
+            newTest.name = new_test_name?;
+            let a = diesel::insert(&newTest).into(test_name::table)
+                .get_result::<TestName>(&connection)
+                .expect("Error adding new test to database");
+            
+            
+            let c = serde_json::to_value::<TestName>(a).unwrap();
+            
+            println!("{:?}", c);
+            
+            return Ok(c)
+        }
+
+        let d = serde_json::from_str("Test is already added").unwrap();
+        println!("{:?}", d);
+        Ok(d)
+	}
+}
