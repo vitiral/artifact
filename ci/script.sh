10d44aa3882b91c0eb0d0618a53489fb752a27e5
Garrett Berg <vitiral@gmail.com>
2017-06-19 14:54:45 -0600

    use env in tests.

--- ci/script.sh
+++ ci/script.sh
@@ -1,17 +1,22 @@
 # This script takes care of testing your crate
 
+source env
+
 set -ex
 
 main() {
-    export RUST_BACKTRACE=1
-    cross build --target $TARGET --verbose
+    #export RUST_BACKTRACE=1
+    #cross build --target $TARGET --verbose
+    just build
 
     if [ ! -z $DISABLE_TESTS ]; then
         return
     fi
 
-    cross test --lib --target $TARGET
-    cross run --target $TARGET -- check
+    #cross test --lib --target $TARGET
+    #cross run --target $TARGET -- check
+    just test-all
+    just run -- check
 
     ## sanity check the file type
     #file target/$TARGET/debug/art
