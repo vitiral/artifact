560a9aad9e8d33f0e2b6e70d6d6e07b752ab2a3d
Garrett Berg <googberg@gmail.com>
2017-11-03 19:19:44 -0600

    fix #190: update licenses to dual MIT+Apache

--- src/user/mod.rs
+++ src/user/mod.rs
@@ -1,19 +1,10 @@
-/*  artifact: the requirements tracking tool made for developers
- * Copyright (C) 2017  Garrett Berg <@vitiral, vitiral@gmail.com>
+/* Copyright (c) 2017 Garrett Berg, vitiral@gmail.com
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the Lesser GNU General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the Lesser GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * */
+ * Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+ * http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+ * http://opensource.org/licenses/MIT>, at your option. This file may not be
+ * copied, modified, or distributed except according to those terms.
+ */
 //! This module defines the loading and saving of artifacts from user data
 
 use time;
