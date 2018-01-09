9167f5e5a0ead8d6b8d738b9777efeec33f6f1aa
Garrett Berg <googberg@gmail.com>
2018-01-08 22:28:18 -0700

    disable basically all travis tests except `cargo test`

--- ci/script.sh
+++ ci/script.sh
@@ -14,16 +14,16 @@ main() {
     fi
 
     export RUST_BACKTRACE=1
-    just lint
-    just lint -- "--features beta"
+    # TODO: just lint
+    # TODO: just lint -- "--features beta"
     cargo test
-    cargo test --features beta
+    # TODO: cargo test --features beta
     # same command that is used in release
-    cross rustc --bin art --target $TARGET --release -- -C lto
-    export TARGET_BIN="target/$TARGET/release/art"
+    # TODO: cross rustc --bin art --target $TARGET --release -- -C lto
+    # TODO: export TARGET_BIN="target/$TARGET/release/art"
     # test "$(uname)" = "Darwin" && echo "TODO: selenium timeout issue on mac" || \
     #     py.test web-ui/sel_tests
-    just check-fmt
+    # TODO: just check-fmt
     # eval "$TARGET_BIN check"
 }
 
