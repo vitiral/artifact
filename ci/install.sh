e4f5d4b3fa6a590dca118ab41b7d7304a1725a66
Garrett Berg <vitiral@gmail.com>
2018-07-16 15:21:19 -0600

    install cargo-web through cargo

--- ci/install.sh
+++ ci/install.sh
@@ -43,19 +43,20 @@ main() {
            --tag $tag \
            --target $target
 
-    # CARGO WEB
-    CARGO_WEB_RELEASE=$(curl -L -s -H 'Accept: application/json' https://github.com/koute/cargo-web/releases/latest)
-    CARGO_WEB_VERSION=$(echo $CARGO_WEB_RELEASE | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
-    CARGO_WEB_URL="https://github.com/koute/cargo-web/releases/download/$CARGO_WEB_VERSION/cargo-web-x86_64-unknown-linux-gnu.gz"
+    # # CARGO WEB
+    # CARGO_WEB_RELEASE=$(curl -L -s -H 'Accept: application/json' https://github.com/koute/cargo-web/releases/latest)
+    # CARGO_WEB_VERSION=$(echo $CARGO_WEB_RELEASE | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
+    # CARGO_WEB_URL="https://github.com/koute/cargo-web/releases/download/$CARGO_WEB_VERSION/cargo-web-x86_64-unknown-linux-gnu.gz"
 
-    echo "Downloading cargo-web from: $CARGO_WEB_URL"
-    curl -L $CARGO_WEB_URL | gzip -d > cargo-web
-    chmod +x cargo-web
+    # echo "Downloading cargo-web from: $CARGO_WEB_URL"
+    # curl -L $CARGO_WEB_URL | gzip -d > cargo-web
+    # chmod +x cargo-web
 
-    mkdir -p ~/.cargo/bin
-    mv cargo-web ~/.cargo/bin
+    # mkdir -p ~/.cargo/bin
+    # mv cargo-web ~/.cargo/bin
 
     # MDBOOK
+    cargo install cargo-web || echo "cargo-web already installed"
     cargo install mdbook || echo "mdbook already installed"
 }
 
