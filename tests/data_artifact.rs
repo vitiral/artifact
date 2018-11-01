ffa30d5ca58bb0e64bb3b17f6fc54622b770f924
Rett Berg <rett@google.com>
2018-11-01 12:18:43 -0700

    fix #238: correct license comments in source code to MIT/APACHE

--- tests/data_artifact.rs
+++ tests/data_artifact.rs
@@ -1,18 +1,18 @@
 /*  artifact: the requirements tracking tool made for developers
- * Copyright (C) 2017  Garrett Berg <@vitiral, vitiral@gmail.com>
+ * Copyright (C) 2018 Rett Berg <@vitiral, vitiral@gmail.com>
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the Lesser GNU General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * The source code is Licensed under either of
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * * Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or
+ *   http://www.apache.org/licenses/LICENSE-2.0)
+ * * MIT license ([LICENSE-MIT](LICENSE-MIT) or
+ *   http://opensource.org/licenses/MIT)
  *
- * You should have received a copy of the Lesser GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * at your option.
+ *
+ * Unless you explicitly state otherwise, any contribution intentionally submitted
+ * for inclusion in the work by you, as defined in the Apache-2.0 license, shall
+ * be dual licensed as above, without any additional terms or conditions.
  * */
 //! Unit/Fuzz Tests:
 //! - #TST-unit.artifact
--- tests/data_interop.rs
+++ tests/data_interop.rs
@@ -1,3 +1,19 @@
+/*  artifact: the requirements tracking tool made for developers
+ * Copyright (C) 2018 Rett Berg <@vitiral, vitiral@gmail.com>
+ *
+ * The source code is Licensed under either of
+ *
+ * * Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or
+ *   http://www.apache.org/licenses/LICENSE-2.0)
+ * * MIT license ([LICENSE-MIT](LICENSE-MIT) or
+ *   http://opensource.org/licenses/MIT)
+ *
+ * at your option.
+ *
+ * Unless you explicitly state otherwise, any contribution intentionally submitted
+ * for inclusion in the work by you, as defined in the Apache-2.0 license, shall
+ * be dual licensed as above, without any additional terms or conditions.
+ * */
 //! Interop Tests:
 //! - #TST-read-artifact
 #[macro_use]
