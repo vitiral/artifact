9f3bbe7df36212c64bf97576e525447cee74bfaa
Rett Berg <rett@google.com>
2018-12-13 14:09:56 -0800

    cargo again

--- ci/install.sh
+++ ci/install.sh
@@ -1,14 +1,14 @@
 set -ex
 
 main() {
-    local target=
-    if [ $TRAVIS_OS_NAME = linux ]; then
-        target=x86_64-unknown-linux-musl
-        sort=sort
-    else
-        target=x86_64-apple-darwin
-        sort=gsort  # for `sort --sort-version`, from brew's coreutils.
-    fi
+    # local target=
+    # if [ $TRAVIS_OS_NAME = linux ]; then
+    #     target=x86_64-unknown-linux-musl
+    #     sort=sort
+    # else
+    #     target=x86_64-apple-darwin
+    #     sort=gsort  # for `sort --sort-version`, from brew's coreutils.
+    # fi
 
     # Builds for iOS are done on OSX, but require the specific target to be
     # installed.
@@ -30,21 +30,21 @@ main() {
             ;;
     esac
 
-    # This fetches latest stable release
-    local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \
-                       | cut -d/ -f3 \
-                       | grep -E '^v[0.1.0-9.]+$' \
-                       | $sort --version-sort \
-                       | tail -n1)
-    curl -LSfs https://japaric.github.io/trust/install.sh | \
-        sh -s -- \
-           --force \
-           --git japaric/cross \
-           --tag $tag \
-           --target $target
+    # # This fetches latest stable release
+    # local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \
+    #                    | cut -d/ -f3 \
+    #                    | grep -E '^v[0.1.0-9.]+$' \
+    #                    | $sort --version-sort \
+    #                    | tail -n1)
+    # curl -LSfs https://japaric.github.io/trust/install.sh | \
+    #     sh -s -- \
+    #        --force \
+    #        --git japaric/cross \
+    #        --tag $tag \
+    #        --target $target
 
-    cross install cargo-web || echo "cargo-web already installed"
-    cross install mdbook || echo "mdbook already installed"
+    cargo install cargo-web || echo "cargo-web already installed"
+    cargo install mdbook || echo "mdbook already installed"
 }
 
 main
