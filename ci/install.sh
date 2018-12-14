54ac5ab4c1e556c92adf3ee09a5d334ce619ecda
Rett Berg <rett@google.com>
2018-12-13 22:35:30 -0800

    backtrace

--- ci/install.sh
+++ ci/install.sh
@@ -1,5 +1,40 @@
 set -ex
 
+# install_github_release USER NAME EXT
+install_github_release() {
+    USER=$1
+    NAME=$2
+    EXT=$3
+
+    URL=https://github.com/$USER/$NAME/releases
+
+    APP_RELEASE=$(curl -L -s -H 'Accept: application/json' $URL/latest)
+    APP_VERSION=$(echo $APP_RELEASE | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
+    APP_FNAME=$NAME-$TARGET.$EXT
+    APP_URL="$URL/download/$APP_VERSION/$APP_FNAME"
+
+    echo "Downloading $NAME from: $APP_URL"
+    curl -L $APP_URL | gzip -d > $NAME
+    chmod +x $NAME
+
+    mkdir -p ~/.cargo/bin
+    mv $NAME ~/.cargo/bin
+}
+
+install_cargo_web() {
+    CARGO_WEB_RELEASE=$(curl -L -s -H 'Accept: application/json' https://github.com/koute/cargo-web/releases/latest)
+    CARGO_WEB_VERSION=$(echo $CARGO_WEB_RELEASE | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
+    CARGO_WEB_URL="https://github.com/koute/cargo-web/releases/download/$CARGO_WEB_VERSION/cargo-web-x86_64-unknown-linux-gnu.gz"
+
+    echo "Downloading cargo-web from: $CARGO_WEB_URL"
+    curl -L $CARGO_WEB_URL | gzip -d > cargo-web
+    chmod +x cargo-web
+
+    mkdir -p ~/.cargo/bin
+    mv cargo-web ~/.cargo/bin
+}
+
+
 main() {
     # local target=
     # if [ $TRAVIS_OS_NAME = linux ]; then
@@ -50,6 +85,13 @@ main() {
 
     cargo install cargo-web --debug || echo "cargo-web already installed"
     cargo install mdbook --debug || echo "mdbook already installed"
+
+    # if [ $TRAVIS_OS_NAME = windows ]; then
+    #     install_github_release rust-lang-nursery mdbook "zip"
+    # else
+    #     install_github_release koute cargo-web "gz"
+    #     install_github_release rust-lang-nursery mdbook "tgz"
+    # fi
 }
 
 main
